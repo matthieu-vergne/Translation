3f9d196a3620b27bffe59245f286fa71c7f62ca5
Sazaju HITOKAGE <sazaju@gmail.com>
2014-01-31 01:30:58 +0100

    Add the map title above the content.

--- src/com/vh/gui/MapContentPanel.java
+++ src/com/vh/gui/MapContentPanel.java
@@ -1,8 +1,11 @@
 package com.vh.gui;
 
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.io.File;
 import java.io.IOException;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -15,25 +18,41 @@ import com.vh.translation.TranslationMap;
 @SuppressWarnings("serial")
 public class MapContentPanel extends JPanel {
 
-	private final JTextArea mapContent;
+	private final JTextArea mapContentArea;
 	private TranslationMap map;
+	private final JLabel mapTitleField;
 
 	public MapContentPanel() {
-		setLayout(new GridLayout(1, 1));
 		setBorder(new EtchedBorder());
-		mapContent = new JTextArea();
-		add(new JScrollPane(mapContent));
+
+		setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.anchor = GridBagConstraints.CENTER;
+		mapTitleField = new JLabel(" ");
+		add(mapTitleField, constraints);
+
+		constraints.gridy = 1;
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1;
+		constraints.weighty = 1;
+		mapContentArea = new JTextArea();
+		add(new JScrollPane(mapContentArea), constraints);
 	}
 
 	public void setMap(TranslationMap map) {
 		String content;
+		File mapFile = map.getBaseFile();
 		try {
-			content = FileUtils.readFileToString(map.getBaseFile());
+			content = FileUtils.readFileToString(mapFile);
 		} catch (IOException e) {
 			throw new IllegalStateException(e);
 		}
-		mapContent.setText(content);
-		mapContent.setCaretPosition(0);
+		mapTitleField.setText(mapFile.getName());
+		mapContentArea.setText(content);
+		mapContentArea.setCaretPosition(0);
 		this.map = map;
 	}
 
