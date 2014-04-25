63e4d3a5355abe3fb5d885fa927278721888ae98
Sazaju HITOKAGE <sazaju@gmail.com>
2014-04-25 04:27:09 +0200

    Update todo.

--- src/main/java/fr/sazaju/vheditor/translation/impl/TranslationUtil.java
+++ src/main/java/fr/sazaju/vheditor/translation/impl/TranslationUtil.java
@@ -27,7 +27,7 @@ import fr.vergne.logging.LoggerConfiguration;
 // TODO identify all the "\." in the original version and place them in the translated version
 // TODO generalize the previous points as a "formatting recovery" feature (take the formatting of the original version and apply/adapt it to the translated version) : identify contents, punctuation and '\.' parts, map the original and translated ones, use the formatting of the original on the translated, adapt if needed
 // TODO wrap the sentences to reach at best the characters limits.
-// TODO use references like https://www.assembla.com/spaces/VH/wiki/Names
+// TODO use references at https://www.assembla.com/spaces/VH/wiki/Reference
 public class TranslationUtil {
 
 	public static final Logger logger = LoggerConfiguration.getSimpleLogger();
