ec4c755c885b3fb0a30064af0ffa9900bef869e0
Sazaju HITOKAGE <sazaju@gmail.com>
2014-04-26 07:42:19 +0200

    Add todo.

--- src/main/java/fr/sazaju/vheditor/gui/MapContentPanel.java
+++ src/main/java/fr/sazaju/vheditor/gui/MapContentPanel.java
@@ -24,6 +24,7 @@ import fr.sazaju.vheditor.translation.impl.TranslationUtil;
 import fr.sazaju.vheditor.translation.impl.backed.BackedTranslationMap;
 import fr.vergne.logging.LoggerConfiguration;
 
+// TODO update list panel info when saving
 @SuppressWarnings("serial")
 public class MapContentPanel extends JPanel {
 
