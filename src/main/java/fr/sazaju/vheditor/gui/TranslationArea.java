13ff7b0407d386472ef175e729933013c5e90216
Sazaju HITOKAGE <sazaju@gmail.com>
2014-04-28 23:34:20 +0200

    Display char limits.

--- src/main/java/fr/sazaju/vheditor/gui/TranslationArea.java
+++ src/main/java/fr/sazaju/vheditor/gui/TranslationArea.java
@@ -1,5 +1,14 @@
 package fr.sazaju.vheditor.gui;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeSet;
+
 import javax.swing.JTextArea;
 import javax.swing.border.EtchedBorder;
 
@@ -9,11 +18,33 @@ import fr.sazaju.vheditor.translation.TranslationEntry;
 public class TranslationArea extends JTextArea {
 
 	private final TranslationEntry entry;
+	private final TreeSet<Integer> limits;
+	private static final List<Color> limitColors = Arrays.asList(Color.RED,
+			Color.LIGHT_GRAY);
 
 	public TranslationArea(TranslationEntry entry) {
 		super(entry.getTranslatedVersion());
 		this.entry = entry;
+		limits = new TreeSet<Integer>(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer i1, Integer i2) {
+				return i2.compareTo(i1);
+			}
+		}) {
+			@Override
+			public boolean add(Integer e) {
+				if (e == null) {
+					return true;
+				} else {
+					return super.add(e);
+				}
+			}
+		};
+		limits.add(entry.getCharLimit(false));
+		limits.add(entry.getCharLimit(true));
 		setBorder(new EtchedBorder());
+		setFont(new Font("monospaced", Font.PLAIN, getFont().getSize()));
 	}
 
 	public void save() {
@@ -26,5 +57,19 @@ public class TranslationArea extends JTextArea {
 
 	public boolean isModified() {
 		return !getText().equals(entry.getTranslatedVersion());
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		int charWidth = g.getFontMetrics().charWidth('m');
+		Iterator<Integer> limitIterator = limits.iterator();
+		Iterator<Color> colorIterator = limitColors.iterator();
+		while (limitIterator.hasNext()) {
+			Integer limit = charWidth * limitIterator.next();
+			g.setColor(colorIterator.next());
+			g.drawLine(limit, 0, limit, getHeight());
+		}
 	}
 }
