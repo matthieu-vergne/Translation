cda4633f5febf365ca0694737283ba40b7860458
Sazaju HITOKAGE <sazaju@gmail.com>
2014-12-19 00:21:20 +0100

    Add persistence and source configuration for labels loading (issue #27).

--- src/main/java/fr/sazaju/vheditor/gui/MapListPanel.java
+++ src/main/java/fr/sazaju/vheditor/gui/MapListPanel.java
@@ -58,8 +58,6 @@ import javax.swing.tree.TreeCellRenderer;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 
-import org.apache.commons.io.FileUtils;
-
 import fr.sazaju.vheditor.gui.parsing.MapLabelPage;
 import fr.sazaju.vheditor.gui.parsing.MapRow;
 import fr.sazaju.vheditor.gui.parsing.MapTable;
@@ -72,13 +70,17 @@ import fr.vergne.parsing.layer.exception.ParsingException;
 @SuppressWarnings("serial")
 public class MapListPanel extends JPanel {
 
+	private static final String CONFIG_LABEL_SOURCE = "labelSource";
+	private static final String CONFIG_LABEL_PREFIX = "label.";
+	private static final String CONFIG_LABEL_LAST_UPDATE = "lastLabelUpdate";
 	private static final String CONFIG_CLEARED_DISPLAYED = "clearedDisplayed";
 	private static final String CONFIG_LABELS_DISPLAYED = "labelsDisplayed";
 	private static final String CONFIG_MAP_DIR = "mapDir";
 	public static final Logger logger = LoggerConfiguration.getSimpleLogger();
 	private final JTextField folderPathField = new JTextField();
 	private final JTree tree;
-	private Map<String, String> mapLabels = null;
+	private final Map<String, String> mapLabels = Collections
+			.synchronizedMap(new HashMap<String, String>());
 	private final Map<File, MapDescriptor> mapDescriptors = Collections
 			.synchronizedMap(new HashMap<File, MapDescriptor>());
 	private int displayedDescriptors = 0;
@@ -153,6 +155,52 @@ public class MapListPanel extends JPanel {
 				.isLabelDisplayed());
 		displayLabels.setToolTipText("Display maps' English labels.");
 		buttons.add(displayLabels);
+
+		final JButton labelSource = new JButton();
+		labelSource.setAction(new AbstractAction("Source") {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String source = Gui.config.getProperty(CONFIG_LABEL_SOURCE);
+				Object answer = JOptionPane
+						.showInputDialog(
+								MapListPanel.this,
+								"Please provide the location of the page describing the labels:",
+								"Label Source", JOptionPane.QUESTION_MESSAGE,
+								null, null, source);
+				if (answer == null || source.equals(answer)) {
+					// no change requested
+				} else if (((String)answer).isEmpty()) {
+					displayError("An empty location is of no use, so the change is cancelled.");
+				} else {
+					logger.info("Label source set: " + answer);
+					Gui.config.setProperty(CONFIG_LABEL_SOURCE,
+							answer.toString());
+					Gui.config.setProperty(CONFIG_LABEL_LAST_UPDATE, "" + 0);
+				}
+			}
+		});
+		labelSource
+				.setToolTipText("Configure the source where to load the maps' labels from.");
+		buttons.add(labelSource);
+
+		final JButton updateLabels = new JButton();
+		updateLabels.setAction(new AbstractAction("Update") {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					loadLabels(true);
+					mapLabels.clear();
+					refreshTree(true);
+				} catch (Exception e) {
+					displayError(e.getMessage());
+				}
+			}
+		});
+		updateLabels
+				.setToolTipText("Request the update of the labels from the label source.");
+		buttons.add(updateLabels);
 
 		return buttons;
 	}
@@ -425,96 +473,104 @@ public class MapListPanel extends JPanel {
 	protected String retrieveMapLabel(File mapFile) {
 		String mapName = mapFile.getName();
 		logger.finest("Retrieving label for " + mapName + "...");
-		if (mapLabels != null) {
-			// reuse the loaded labels
+		if (mapLabels.containsKey(mapName)) {
+			// already loaded
 		} else {
-			mapLabels = new HashMap<String, String>();
-			MapLabelPage mapLabelPage = new MapLabelPage();
-			// TODO set remote/local more smartly
-			boolean fromRemote = true;
-			String pageContent;
-			Object pageSource;
-			if (fromRemote) {
-				try {
-					pageSource = new URL(
-							"https://www.assembla.com/spaces/VH/wiki/Map_List");
-				} catch (MalformedURLException e) {
-					throw new RuntimeException(e);
-				}
-			} else {
-				pageSource = new File("/media/Sazaju/Downloads/Map List.html");
-			}
-
-			logger.info("Loading page from " + pageSource + "...");
 			try {
-				if (pageSource instanceof URL) {
-					pageContent = loadMapLabelsPageFromWeb((URL) pageSource);
-				} else if (pageSource instanceof File) {
-					pageContent = FileUtils.readFileToString((File) pageSource);
-				} else {
-					throw new RuntimeException("Unmanaged type of source: "
-							+ pageSource.getClass());
-				}
+				loadLabels(false);
 			} catch (Exception e) {
-				String message = "Impossible to read the source " + pageSource;
-				JOptionPane.showConfirmDialog(this, message, "Loading Failed",
-						JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null);
-				throw new RuntimeException(message, e);
+				e.printStackTrace();
 			}
-
-			logger.info("Parsing content...");
-			try {
-				mapLabelPage.setContent(pageContent);
-			} catch (ParsingException e) {
-				String message = "Impossible to find map labels in "
-						+ pageSource;
-				logger.warning(message);
-				JOptionPane.showConfirmDialog(this, message, "Loading Failed",
-						JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null);
-				return null;
-			}
-
-			logger.info("Searching for labels...");
-			MapTable table = mapLabelPage.getTable();
-			int total = table.size();
-			for (MapRow row : table) {
-				mapLabels.put("Map" + row.getId() + ".txt",
-						row.getEnglishLabel());
-			}
-			logger.info("Labels loaded: " + total);
+			mapLabels
+					.put(mapName,
+							Gui.config.getProperty(CONFIG_LABEL_PREFIX
+									+ mapName, null));
 		}
 		String mapLabel = mapLabels.get(mapName);
 		logger.finest("Label retrieved: " + mapName + " = " + mapLabel);
 		return mapLabel;
 	}
 
-	private String loadMapLabelsPageFromWeb(URL pageUrl) {
+	private void loadLabels(boolean force) {
+		if (Gui.config.containsKey(CONFIG_LABEL_SOURCE)) {
+			// source already configured
+		} else {
+			Gui.config.setProperty(CONFIG_LABEL_SOURCE,
+					"https://www.assembla.com/spaces/VH/wiki/Map_List");
+		}
+		long lastUpdate = Long.parseLong(Gui.config.getProperty(
+				CONFIG_LABEL_LAST_UPDATE, "0"));
+		if (!force && System.currentTimeMillis() < lastUpdate + 86400000) {
+			// not old enough
+		} else {
+			String source = Gui.config.getProperty(CONFIG_LABEL_SOURCE);
+			URL url;
+			try {
+				url = new URL(source);
+			} catch (MalformedURLException e) {
+				try {
+					url = new File(source).toURI().toURL();
+				} catch (MalformedURLException e1) {
+					throw new RuntimeException("Malformed URL: " + source, e);
+				}
+			}
+			try {
+				loadLabelsFrom(url);
+			} finally {
+				Gui.config.setProperty(CONFIG_LABEL_LAST_UPDATE,
+						"" + System.currentTimeMillis());
+			}
+		}
+	}
+
+	private void loadLabelsFrom(URL pageUrl) {
+		logger.info("Loading page from " + pageUrl + "...");
 		String pageContent;
 		try {
-			URLConnection con = pageUrl.openConnection();
-			Pattern p = Pattern.compile("text/html;\\s+charset=([^\\s]+)\\s*");
-			Matcher m = p.matcher(con.getContentType());
-			/*
-			 * If Content-Type doesn't match this pre-conception, choose default
-			 * and hope for the best.
-			 */
-			String charset = m.matches() ? m.group(1) : "ISO-8859-1";
-			Reader r = new InputStreamReader(con.getInputStream(), charset);
-			StringBuilder buf = new StringBuilder();
-			while (true) {
-				int ch = r.read();
-				if (ch < 0)
-					break;
-				buf.append((char) ch);
+			URLConnection connection = pageUrl.openConnection();
+			Pattern pattern = Pattern
+					.compile("text/html;\\s+charset=([^\\s]+)\\s*");
+			Matcher matcher = pattern.matcher(connection.getContentType());
+			String charset = matcher.matches() ? matcher.group(1) : "UTF-8";
+			Reader reader = new InputStreamReader(connection.getInputStream(),
+					charset);
+			StringBuilder buffer = new StringBuilder();
+			int ch;
+			while ((ch = reader.read()) >= 0) {
+				buffer.append((char) ch);
 			}
-			pageContent = buf.toString();
-			r.close();
-		} catch (MalformedURLException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+			pageContent = buffer.toString();
+			reader.close();
+		} catch (Exception e) {
+			throw new RuntimeException("Impossible to read the source "
+					+ pageUrl, e);
 		}
-		return pageContent;
+
+		logger.info("Parsing content...");
+		MapLabelPage mapLabelPage = new MapLabelPage();
+		try {
+			mapLabelPage.setContent(pageContent);
+		} catch (ParsingException e) {
+			throw new RuntimeException("Impossible to find map labels in "
+					+ pageUrl, e);
+		}
+		logger.info("Content parsed.");
+
+		logger.info("Saving labels...");
+		MapTable table = mapLabelPage.getTable();
+		int total = table.size();
+		for (MapRow row : table) {
+			String name = "Map" + row.getId() + ".txt";
+			String label = row.getEnglishLabel();
+			Gui.config.setProperty(CONFIG_LABEL_PREFIX + name, label);
+		}
+		logger.info("Labels saved: " + total);
+	}
+
+	private void displayError(String message) {
+		JOptionPane.showOptionDialog(this, message, "Loading Failed",
+				JOptionPane.PLAIN_MESSAGE, JOptionPane.QUESTION_MESSAGE, null,
+				new Object[] { "OK" }, "OK");
 	}
 
 	private File getSelectedFile(final JTree tree) {
